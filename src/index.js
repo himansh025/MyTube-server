c3232718bd1161d3cca8786c547c8bd98abbc751
himansh025 <himanshudeolia0825@gmail.com>
2024-10-17 19:45:00 +0530

    cors set for backend

--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
-
-
 import dotenv from 'dotenv';
 import connectDB from './DB/index.js';
-import  app  from './app.js';
+import app from './app.js';
 
-// // Load environment variables from .env file
+// Load environment variables from .env
 dotenv.config({
     path: './.env',
 });
 
-
 // Connect to the database
-connectDB()
-.then(()=>{
-    app.listen(process.env.PORT || 8000 , ()=>{
-        console.log(`Server started at port ${process.env.PORT}`)
-    })
-})
-.catch((err)=>{
-    console.log("Mongodb connection failed :: " , err)
-})
+connectDB().then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server started at port ${process.env.PORT}`);
+    });
+}).catch((err) => {
+    console.log("Mongodb connection failed: ", err);
+});
