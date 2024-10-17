62e93bffd459b86bc01cb6bf560af76d19a749a3
himansh025 <himanshudeolia0825@gmail.com>
2024-10-17 21:47:48 +0530

    cors set for backend

---
+++ functions/server.js
@@ -0,0 +1,9 @@
+// functions/getAllVideos.js
+exports.handler = async (event, context) => {
+    // your backend logic
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Here the server" }),
+    };
+  };
+  
