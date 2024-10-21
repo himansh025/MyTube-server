9d272d578bbeeac91f0f1bddefe9abb117f06506
himansh025 <himanshudeolia0825@gmail.com>
2024-10-21 13:26:20 +0530

    subs

--- src/app.js
+++ src/app.js
@@ -34,7 +34,7 @@ import videoRouter from './routes/video.routes.js'
 import likeRouter from './routes/like.routes.js'
 import tweetRouter from './routes/tweet.routes.js'
 // import subscriptionRouter from './routes/subscription.routes.js'
-import dashboardRouter from './routes/dashboard.routes.js'
+// import dashboardRouter from './routes/dashboard.routes.js'
 import playlistRouter from './routes/playlist.routes.js'
 
 
@@ -44,7 +44,8 @@ app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/tweet", tweetRouter)
 // app.use("/api/v1/subs", subscriptionRouter);
-// app.use("/api/v1/dashboard", dashboardRouter);
+// app.use("/api/v1/dashboard", dashboardRouter);y
+
 app.use("/api/v1/playlist", playlistRouter);
 
 export default app;
