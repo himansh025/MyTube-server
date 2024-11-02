6ab6d0ed32eed0a2f8f153552c6ddedb4d02233d
himansh025 <himanshudeolia0825@gmail.com>
2024-11-02 20:57:36 +0530

    fix the bugs

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,16 +1,18 @@
-import{ mongoose, Schema} from 'mongoose'
+import mongoose, { Schema } from 'mongoose';
 
-const SubscriptionModel= new Schema({
-subscriber:{
+const SubscriptionModel = new Schema({
+  subscriber: {
     type: Schema.Types.ObjectId,
-    // who is subscriber
-    ref:'User'
-},
-channel:{
+    // who is subscribing
+    ref: 'User',
+  },
+  channel: {
     type: Schema.Types.ObjectId,
-    // onw whom to subscribing
-    ref:'User'
-}
-},{timestamps: true})
+    // whom they are subscribing to
+    ref: 'User',
+  },
+}, { timestamps: true });
 
-export const Subscription = mongoose.model("Subscription",SubscriptionModel) 
+const Subscription = mongoose.model("Subscription", SubscriptionModel);
+
+export default Subscription;
